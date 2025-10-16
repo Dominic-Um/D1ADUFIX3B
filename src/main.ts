c6fe247dfa03d4daeb69b90af52a68f47cd0d84b
Dominic-Um <dumbrasa@ucsc.edu>
2025-10-16 03:46:33 +0000

    Added clickable button

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <button id="iconButton">
+    <img src="${exampleIconUrl}" alt="Paperclip Icon" />
+  </button>
 `;
